082472cdd68f1ef3340b68961a385dd2e7d6171e
gnismas <csamsinges@gmail.com>
2022-09-08 13:42:16 +0200

    first

---
+++ src/main/java/com/example/dockza/DockzaApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dockza/DockzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
